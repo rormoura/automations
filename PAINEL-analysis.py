9c5c7f13552e8b55fb4775023622f188808ed30f
Rodrigo Moura <rrm2@cin.ufpe.br>
2024-05-02 10:15:51 -0300

    Add painel-de-precos file

--- PAINEL-analysis.py
+++ PAINEL-analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 def weighted_average (values, quantities):
     result = 0
@@ -6,9 +7,26 @@ def weighted_average (values, quantities):
         result += float(values[i])*quantities[i]
     return result/sum(quantities);
 
-filePath = "C:/Users/rormo/Downloads/painel_precos.csv"
+def is_positive_integer(user_input):
+    if re.match(r'^[1-9]\d*$', user_input):
+        return True
+    else:
+        return False
 
-df = pd.read_csv(filePath, delimiter=";", skiprows=1, encoding='latin1')
-pd.set_option('display.max_columns', None)
-print(df)
-#print(weighted_average(list(map(lambda x: x.replace(',', '.'), df['Preço Unitário'].tolist())), df['Qtd Itens Comprados'].tolist()))
+while(True):
+
+    filesNumber = input("Quantos arquivos Excel (.xlsx) você deseja enviar?")
+
+    if(is_positive_integer(filesNumber)):
+        filePath = input("Digite o caminho para o arquivo 1:")
+        df = pd.read_excel(filePath, skiprows=4)
+        for i in range(1, filesNumber):
+            filePath = input("Digite o caminho para o arquivo "+(i+1)+":")
+            df = pd.read_excel(filePath, skiprows=4)
+        
+        pd.set_option('display.max_columns', None)
+        print(df)
+        break
+    else:
+        print("Número inválido de arquivos. Digite novamente.")
+#print(weighted_average(list(map(lambda x: x.replace(',', '.'), df['Valor Unitário'].tolist())), df['Quantidade Ofertada'].tolist()))
