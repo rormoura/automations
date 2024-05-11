7b5c9eea453dddfccd91fa4098bcbe1970d4ac06
MatheusXimenesFerreira <matheus.ximenes.ferreira@gmail.com>
2024-05-10 23:27:39 -0300

    fix chauvenet function

--- methodologies/chauvenet.py
+++ methodologies/chauvenet.py
@@ -3,19 +3,26 @@ import numpy as np
 from scipy.stats import norm
 
 def chauvenet(df):
-        count = 0
+
         df = df.to_frame()
         cv = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
-        while (cv(df[df.columns[0]]) > 25):
+        
+        breakValue = df[df.columns[0]].mean()
+ 
+        while ((cv(df[df.columns[0]]) > 25)) :
 
-                mean = df[df.columns[0]].mean()
-                std_dev = df[df.columns[0]].std()
-                normValue = norm.ppf(1-1/(4*len(df[df.columns[0]])))
+            mean = df[df.columns[0]].mean()
+            std_dev = df[df.columns[0]].std()
+            normValue = norm.ppf(1-1/(4*len(df[df.columns[0]])))
 
-                minValue = np.abs(mean - (normValue * std_dev))
-                maxValue = mean + (normValue*std_dev)
+            minValue = (mean - (normValue * std_dev))
+            maxValue = mean + (normValue*std_dev)
 
-                df = df[df[df.columns[0]] >= minValue] 
-                df = df[df[df.columns[0]] <= maxValue]
+            df = df[df[df.columns[0]] >= minValue]
+            df = df[df[df.columns[0]] <= maxValue]
+
+            if (breakValue == df[df.columns[0]].mean()):
+                break
+            breakValue = df[df.columns[0]].mean()
 
         return df[df.columns[0]].mean()
