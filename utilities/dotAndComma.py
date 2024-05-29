6e44579070c2e4a653c707efb4d51ee9a8e28ad0
Rodrigo Moura <rrm2@cin.ufpe.br>
2024-05-29 09:32:06 -0300

    Fix ',' handling at dotAndComma

--- utilities/dotAndComma.py
+++ utilities/dotAndComma.py
@@ -1,10 +1,12 @@
-#This function receives a dataframe column and returns a dataframe column.
-#It takes each line, which is a number, from the column and:
-#   1 - If there is a dot, it removes the dot.
-#   2 - If there is a comma, it replaces the comma with a dot.
+import pandas as pd
 
 def dotAndComma (df):
     if(df.dtype == object):
-        df = df.apply(lambda x: float(x.replace('.', '')) if ('.' in x) else x)
-        df = df.apply(lambda x: float(x.replace(',', '.')) if (',' in x) else x)
+        elements = []
+        for x in df:
+            x = str(x)
+            if(',' in x):
+                x = x.replace(',', '.')
+            elements.append(float(x))
+        return pd.Series(elements)
     return df
