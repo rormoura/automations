6fc80b0e6c6ccd7bba796d350c7e4e508293ae99
Rodrigo Moura <rrm2@cin.ufpe.br>
2024-06-03 10:07:58 -0300

    Add '.' handling at to dotAndComma.py

--- utilities/dotAndComma.py
+++ utilities/dotAndComma.py
@@ -10,3 +10,14 @@ def dotAndComma (df):
             elements.append(float(x))
         return pd.Series(elements)
     return df
+
+def dot (df):
+    if(df.dtype == object):
+        elements = []
+        for x in df:
+            x = str(x)
+            if('.' in x):
+                x = x.replace('.', '')
+            elements.append(float(x))
+        return pd.Series(elements)
+    return df
