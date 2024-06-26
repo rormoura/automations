ce57d347313a92f5e1c6024298345b49c6758d5f
MatheusXimenesFerreira <matheus.ximenes.ferreira@gmail.com>
2024-06-25 23:49:57 -0300

    fix cleanData function

--- utilities/apiOpenData.py
+++ utilities/apiOpenData.py
@@ -1,15 +1,19 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 import pytz
 
 def cleanData(lista_dicionarios):
     
     df = pd.DataFrame(lista_dicionarios)
-    df['dataCompra'] = pd.to_datetime(df['dataCompra'], format='ISO8601')
     df = df[df['precoUnitario'] != 0]
+    df = df.drop_duplicates(subset='idCompra', keep='first')
+    df['dataCompra'] = pd.to_datetime(df['dataCompra'])
     data_limite = datetime.now(pytz.UTC) - timedelta(days=11*30)
     df = df[df['dataCompra'] >= data_limite]
+    df['dataCompra'] = df['dataCompra'].dt.tz_convert('America/Sao_Paulo')
+    df['dataCompra'] = df['dataCompra'].dt.strftime('%d/%m/%Y %H:%M:%S')
     df = df.sort_values(by='precoUnitario')
-    df = df.drop_duplicates(subset='idCompra', keep='first')
-    
+    df['Unidade'] = df[['nomeUnidadeFornecimento', 'capacidadeUnidadeFornecimento', 'siglaUnidadeMedida']].apply(lambda row: ' '.join(row.dropna().astype(str)), axis=1)
+
     return df
